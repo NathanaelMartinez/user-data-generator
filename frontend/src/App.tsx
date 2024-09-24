028667631a0b8ac41239847a03c39685e1e7b510
NathanaelMartinez <nathanaellmartinez@gmail.com>
2024-09-24 15:57:26 -0500

    separate out generation page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import Toolbar from './components/Toolbar';
-import UserTable from './components/UserTable';
+// src/App.tsx
+import React from 'react';
+import GeneratorPage from './pages/GeneratorPage';
 
-const App = () => {
-    const [users, setUsers] = useState([]);
-    const [loading, setLoading] = useState(false);
-    
-    const fetchUsers = async (localeId: String, seed: number, errorSize: number) => {
-        setLoading(true);
-        const response = await fetch(`/api/users/${localeId}/${seed}/${errorSize}`);
-        const data = await response.json();
-        setUsers(data);
-        setLoading(false);
-    };
-
-    useEffect(() => {
-        const localeId = 'en_US';
-        const seed = 0;
-        const errorSize = 0;
-
-        fetchUsers(localeId, seed, errorSize);
-    }, []);
-
+const App: React.FC = () => {
     return (
         <div>
-            <h1>Random User Data Generator</h1>
-            <Toolbar onFetchUsers={fetchUsers} />
-            <UserTable users={users} loading={loading} />
+            <GeneratorPage />
         </div>
     );
 };
---
+++ frontend/src/pages/GeneratorPage.tsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+import Toolbar from '../components/Toolbar';
+import UserTable from '../components/UserTable';
+
+const HomePage: React.FC = () => {
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(false);
+    
+    const fetchUsers = async (localeId: string, seed: number, errorSize: number) => {
+        setLoading(true);
+        const response = await fetch(`/api/users/${localeId}/${seed}/${errorSize}`);
+        const data = await response.json();
+        setUsers(data);
+        setLoading(false);
+    };
+
+    useEffect(() => {
+        const localeId = 'en_US';
+        const seed = 0;
+        const errorSize = 0;
+
+        fetchUsers(localeId, seed, errorSize);
+    }, []);
+
+    return (
+        <div>
+            <h1>Random User Data Generator</h1>
+            <Toolbar onFetchUsers={fetchUsers} />
+            <UserTable users={users} loading={loading} />
+        </div>
+    );
+};
+
+export default HomePage;
