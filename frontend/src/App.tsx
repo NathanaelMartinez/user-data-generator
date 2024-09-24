6c65e200aa92e6e91d35e38169a68b3803817658
NathanaelMartinez <nathanaellmartinez@gmail.com>
2024-09-24 15:29:16 -0500

    fetch userData from backend to frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,30 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [users, setUsers] = useState([]);
+
+
+    
+    const fetchUsers = async (localeId: String, seed: number, errorSize: number) => {
+        const response = await fetch(`/api/users/${localeId}/${seed}/${errorSize}`);
+        const data = await response.json();
+        setUsers(data);
+    };
+
+    useEffect(() => {
+        const localeId = 'en_US';
+        const seed = 123234234;
+        const errorSize = 0;
+
+        fetchUsers(localeId, seed, errorSize);
+    }, []);
+
+    return (
+        <div>
+            <h1>Random User Data Generator</h1>
+            <pre>{JSON.stringify(users, null, 2)}</pre>
+        </div>
+    );
+};
 
 export default App;
